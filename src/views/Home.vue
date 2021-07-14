f8113f9d5fb375420bb7e02f7cf6d0298360ba70
Tal Ben David <talbd51@gmail.com>
2021-07-14 13:35:34 +0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,15 @@
 <template>
   <div class="home-page">
-    <div class="hero-img">
-    <h1>HERO</h1>
-    </div>
-    
+    <div class="hero-txt"> <h1>HERO</h1></div>
+      <div class="hero-img">
+        <img src="@/assets/hero.png" alt="">
+        </div>
   </div>
 </template>
 
 <script>
-
-
-
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
