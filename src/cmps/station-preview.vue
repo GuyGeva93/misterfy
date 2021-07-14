79d222120a6e91034893e4181519e6929e6069b9
Guy Geva <guy1030@gmail.com>
2021-07-14 16:55:31 +0300

    Guy: Initial station card

---
+++ src/cmps/station-preview.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="station-preview">
+    <div class="station-preview-card" @click="play">
+      <span class="station-preview-name">{{ station.name }}</span>
+      <img
+        src="https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/6a/e4/59/6ae45956-8b3d-0ff2-81f8-587c7c65b515/source/256x256bb.jpg"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+ props: {
+   station: {
+     type: Object,
+   },
+ },
+
+ methods: {
+   play() {
+     console.log('Play station')
+   }
+ },
+}
+</script>
