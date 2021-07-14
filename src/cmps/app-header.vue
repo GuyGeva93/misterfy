c6fa836d45935e81fdf11810c1032342496c1470
Tal Ben David <talbd51@gmail.com>
2021-07-14 13:08:59 +0300

    initial setup - tal

---
+++ src/cmps/app-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <header>
+      <nav class="main-nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/misterfy">Misterfy</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/misterfy-app.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="misterfy-app">
+
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
