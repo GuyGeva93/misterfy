e77024c63ad361ed0b56ba4ba07a89a9e7388e20
Tal Ben David <talbd51@gmail.com>
2021-07-15 15:46:40 +0300

    .

--- src/cmps/chat.vue
+++ src/cmps/chat.vue
@@ -4,18 +4,18 @@
     <ul class="clear-list msg-list">
       <li v-for="(msg, idx) in msgs" :key="idx">
         <section class="msg-info">
-<h3>{{msg.from.name+msg.from._id}}</h3>
-             <h4> {{formatDate(msg.sentAt)}}</h4>
+          <h3>{{ msg.from.name + msg.from._id }}</h3>
+          <h4>{{ formatDate(msg.sentAt) }}</h4>
         </section>
         <section class="msg-text">
           {{ msg.txt }}
         </section>
       </li>
     </ul>
-      <form @submit.prevent="sendMsg" class="msg-form">
-        <input type="text" v-model="msg.txt" placeholder="Your msg" />
-        <button>Send</button>
-      </form>
+    <form @submit.prevent="sendMsg" class="msg-form">
+      <input type="text" v-model="msg.txt" placeholder="Your msg" />
+      <button>Send</button>
+    </form>
   </section>
 </template>
 
@@ -28,37 +28,37 @@ export default {
       type: String,
     },
   },
+  // async created() {
+  //   socketService.emit("chat topic", this.stationId);
+  //   socketService.on("new msg", this.loadMsgs);
+  //   if (!this.msgs.length) {
+  //     this.msgs = await chatService.query();
+  //   }
+  // },
   async created() {
     socketService.emit("chat topic", this.stationId);
     socketService.on("new msg", this.loadMsgs);
     if (!this.msgs.length) {
-      this.msgs = await chatService.query();
+      this.msgs = await chatService.query(this.stationId);
     }
   },
-    async created(){
-          socketService.emit('chat topic', this.stationId)
-          socketService.on('new msg',this.loadMsgs);
-        if(!this.msgs.length){
-          this.msgs=await chatService.query(this.stationId);
-        }
-    },
   data() {
     return {
       msgs: [],
       msg: chatService.getEmptyMsg(),
     };
   },
-  methods:{
-  async  sendMsg(){
-    this.msg.stationId=this.stationId;
-     await chatService.add(this.msg);
-     this.msgs.push(this.msg);
-     const reply=await chatService.botReply(this.msg);
-     this.msgs.push(reply);
-     this.msg=chatService.getEmptyMsg();
+  methods: {
+    async sendMsg() {
+      this.msg.stationId = this.stationId;
+      await chatService.add(this.msg);
+      this.msgs.push(this.msg);
+      const reply = await chatService.botReply(this.msg);
+      this.msgs.push(reply);
+      this.msg = chatService.getEmptyMsg();
     },
-    loadMsgs(msgs){
-      this.msgs=msgs;
+    loadMsgs(msgs) {
+      this.msgs = msgs;
       // this.msgs=msgs.filter(msg=>msg.station===this.stationId);
     },
     formatDate(timeStamp) {
