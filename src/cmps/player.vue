c10619c57258647b5ea15d773d11036343c6bdfb
Guy Geva <guy1030@gmail.com>
2021-07-16 16:18:23 +0300

    Test

--- src/cmps/player.vue
+++ src/cmps/player.vue
@@ -1,8 +1,13 @@
 <template>
-  <section class="player" v-if="songId">
-    <vue-plyr ref="plyr">
+  <section class="player" v-if="songId" >
+    <vue-plyr>
       <div class="plyr__video-embed" id="player">
-        <iframe :src="src" allowtransparency allow="autoplay"></iframe>
+        <iframe
+          :src="src"
+          allowfullscreen
+          allowtransparency
+          allow="autoplay"
+        ></iframe>
       </div>
       <!-- <div
         ref="plyr"
@@ -14,46 +19,27 @@
 </template>
 
 <script>
-// import { eventBusService } from "@/services/eventBus-service.js";
+
 export default {
+  // created() {
+  //   console.log(this.$store.getters.currSongId)
+  // },
   data() {
     return {
-      currSongId: "",
-      // isPlaying: false,
-    };
-  },
-  methods: {
-    // pause() {
-    //   this.$refs.plyr.player.on("playing", () => {
-    //     this.$refs.plyr.player.pause();
-    //   });
-    // },
-  },
-  mounted() {
-    if (!this.$refs.plyr) return;
-
-    this.$refs.plyr.player.on("ready", () => {
-      // console.log();
-      // console.log(this.$refs);
-      this.$refs.plyr.player.play();
-    });
-    this.$refs.plyr.player.on("ended", () => {
-      this.$refs.plyr.player.play();
-    });
-
+      // currSongId: ''
+      // bTqVqk7FSmY
+    }
   },
 
   computed: {
     songId() {
-      return this.$store.getters.currSongId;
+      return this.$store.getters.currSongId
     },
     src() {
-      return `https://www.youtube.com/embed/${this.songId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`;
-    },
+      return `https://www.youtube.com/embed/${this.songId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`
+    }
   },
-  created() {
-    // this.currSongId = this.$store.getters.currSongId;
-  },
+
 };
 </script>
 
