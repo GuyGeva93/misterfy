4fdf50d9015eb49cbedb2869dbf60ec4c38f126e
Guy Geva <guy1030@gmail.com>
2021-07-17 21:15:09 +0300

    Guy: Improve station add

--- src/cmps/station-add.vue
+++ src/cmps/station-add.vue
@@ -4,11 +4,23 @@
 		<h2>Create your new station</h2>
 		<form @submit.prevent="createStation">
 			<label class="choose-file">
-				<div>
-					<img
-						src="https://images.squarespace-cdn.com/content/v1/504fafcf84ae1835d7e5c807/1481053836481-1P6LXZV7W56MFQ3XTRYF/upload-arrow_318-26670.jpg?format=750w"
-					/>
-				</div>
+				<svg
+					width="70px"
+					aria-hidden="true"
+					focusable="false"
+					data-prefix="fas"
+					data-icon="cloud-upload-alt"
+					class="svg-inline--fa fa-cloud-upload-alt fa-w-20"
+					role="img"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 640 512"
+				>
+					<path
+						fill="currentColor"
+						d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4zM393.4 288H328v112c0 8.8-7.2 16-16 16h-48c-8.8 0-16-7.2-16-16V288h-65.4c-14.3 0-21.4-17.2-11.3-27.3l105.4-105.4c6.2-6.2 16.4-6.2 22.6 0l105.4 105.4c10.1 10.1 2.9 27.3-11.3 27.3z"
+					></path>
+				</svg>
+				<span> Upload file</span>
 				<input type="file" @change="handleImg" />
 			</label>
 			<input type="url" placeholder="Image URL" v-model="imgUrl" />
@@ -19,7 +31,13 @@
 				v-model="newStation.description"
 			/>
 			<!-- <input type="text" placeholder="Genre" v-model="newStation.tags[0]" /> -->
-			<input type="text" v-model="newStation.tags" list="tags" @change="tagsShow"/>
+			<input
+				type="text"
+				v-model="newStation.tags"
+				list="tags"
+				@change="tagsShow"
+				placeholder="Genre"
+			/>
 			<datalist id="tags">
 				<option v-for="tag in tags" :key="tag">{{ tag }}</option>
 			</datalist>
@@ -52,28 +70,28 @@ export default {
 			const file = ev.target.files[0]
 			// this.imgLoaded = false;
 			try {
-				this.imgLoaded = false;
-				const savedImg = await uploadImg(file);
-				this.newStation.imgUrl = savedImg.url;
-				this.imgLoaded = true;
+				this.imgLoaded = false
+				const savedImg = await uploadImg(file)
+				this.newStation.imgUrl = savedImg.url
+				this.imgLoaded = true
 			} catch (error) {
-				console.log(error);
+				console.log(error)
 			}
 		},
 		async createStation() {
 			if (!this.newStation.imgUrl && this.imgUrl) {
 				this.newStation.imgUrl = this.imgUrl;
 			}
-			this.newStation = await this.$store.dispatch({ type: 'saveStation', station: this.newStation });
+			this.newStation = await this.$store.dispatch({ type: 'saveStation', station: this.newStation })
 			this.$emit("closeModal");
-			this.$router.push(`/details/${this.newStation._id}`);
+			this.$router.push(`/details/${this.newStation._id}`)
 		},
 		closeModal() {
-			this.$emit("closeModal");
+			this.$emit("closeModal")
 		},
-    tagsShow(){
-      console.log(this.newStation.tags)
-    }
+		tagsShow() {
+			console.log(this.newStation.tags)
+		}
 	},
 };
 </script>
