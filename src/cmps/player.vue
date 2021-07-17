d30fa924060bbe1519e5c82b281ee0a5f578a269
Guy Geva <guy1030@gmail.com>
2021-07-17 14:09:14 +0300

    Guy: Added vue-marquee-text-component

--- src/cmps/player.vue
+++ src/cmps/player.vue
@@ -1,75 +1,73 @@
 <template>
-  <section class="player" v-if="songId">
-    <section class="player-controls">
-      <span>⏪</span>
-      <span v-if="isPlay" @click.stop="togglePlay">▶</span>
-      <span v-else @click.stop="togglePlay">⏸</span>
-      <span>⏩</span>
-    </section>
-    <vue-plyr ref="plyr">
-      <div class="plyr__video-embed" id="player">
-        <iframe :src="src" allowtransparency allow="autoplay"></iframe>
-      </div>
-      <!-- <div
-        ref="plyr"
-        data-plyr-provider="youtube"
-        :data-plyr-embed-id="songId"
-      ></div> -->
-    </vue-plyr>
-  </section>
+	<section class="player" v-if="songId">
+		<section class="player-controls">
+			<marquee-text>
+				<span>1 2 3 4</span>
+			</marquee-text>
+
+			<span @click.stop="prevSong">⏪</span>
+			<span v-if="isPlay" @click.stop="togglePlay">▶</span>
+			<span v-else @click.stop="togglePlay">⏸</span>
+			<span @click.stop="nextSong">⏩</span>
+		</section>
+		<vue-plyr ref="plyr">
+			<div class="plyr__video-embed" id="player">
+				<iframe :src="src" allowtransparency allow="autoplay"></iframe>
+			</div>
+		</vue-plyr>
+	</section>
 </template>
 
 <script>
+import MarqueeText from 'vue-marquee-text-component'
 export default {
-  data() {
-    return {
-      currSongId: "",
-      isPlay: false
-    };
-  },
-  mounted() {
-    if (!this.$refs.plyr) return;
+	data() {
+		return {
+			currSongId: "",
+			isPlay: false
+		};
+	},
+	mounted() {
+		if (!this.$refs.plyr) return // When player isn't ready
 
-    this.$refs.plyr.player.on("ready", () => {
-      this.$refs.plyr.player.play();
-    });
-    // this.$refs.plyr.player.on("pause", () => {
-    //   console.log('pause')
-    //   this.$refs.plyr.player.pause();
-    // });
-    this.$refs.plyr.player.on("ended", () => {
-      this.$store.commit({ type: 'nextSong' })
-      this.$refs.plyr.player.play();
-    });
-  },
-
-  methods: {
-    togglePlay() {
-      if (this.isPlay) {
-        console.log('play')
-        this.$refs.plyr.player.play()
-      } else {
-        console.log('pause')
-        this.$refs.plyr.player.pause()
-      }
-      this.isPlay = !this.isPlay
-    },
-    // play(songId) {
-    //   this.$store.commit({ type: "loadSongToPlayer", songId });
-    //   eventBusService.$emit("playSong");
-    //   this.isPlaying = true
-    // },
-  },
-  computed: {
-    songId() {
-      return this.$store.getters.currSongId;
-    },
-    src() {
-      return `https://www.youtube.com/embed/${this.songId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`;
-    },
-  },
-
-};
+		this.$refs.plyr.player.on("ready", () => {
+			this.$refs.plyr.player.play()
+		})
+		this.$refs.plyr.player.on("ended", () => {
+			this.$store.commit({ type: 'nextSong' })
+			this.$refs.plyr.player.play()
+		})
+	},
+	methods: {
+		togglePlay() {
+			if (this.isPlay) {
+				console.log('play')
+				this.$refs.plyr.player.play()
+			} else {
+				console.log('pause')
+				this.$refs.plyr.player.pause()
+			}
+			this.isPlay = !this.isPlay
+		},
+		prevSong() {
+			this.$store.commit({ type: 'prevSong' })
+		},
+		nextSong() {
+			this.$store.commit({ type: 'nextSong' })
+		}
+	},
+	computed: {
+		songId() {
+			return this.$store.getters.currSongId;
+		},
+		src() {
+			return `https://www.youtube.com/embed/${this.songId}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1`;
+		},
+	},
+	components: {
+		MarqueeText,
+	},
+}
 </script>
 
 <style>
