fbdefbfabeed25653ccb65eb9ab5b1e24ec65d5d
Guy Geva <guy1030@gmail.com>
2021-07-19 15:30:46 +0300

    Guy: try/catch + working on clean code

--- src/views/explore.vue
+++ src/views/explore.vue
@@ -1,84 +1,88 @@
 <template>
-  <main class="explore-container">
-    <section class="explore">
-      <ul class="tags-list">
-        <!-- <li class="tag-preview">
+	<main class="explore-container">
+		<section class="explore">
+			<ul class="tags-list">
+				<!-- <li class="tag-preview">
           <router-link :to="'/explore/' + this.name" >All</router-link>
         </li> -->
-        <li v-for="(tag, idx) in tags" :key="idx" class="tag-preview">
-          <router-link :to="getUrl(tag)">
-            <station-tag :tag="tag" />
-          </router-link>
-        </li>
-      </ul>
-    </section>
-    <template v-if="!isLoading">
-      <section v-if="stations">
-        <station-list :stations="stations" />
-      </section>
-      <h2 v-else>No stations found</h2>
-    </template>
-    <h2 v-else>Loading...</h2>
-  </main>
+				<li v-for="(tag, idx) in tags" :key="idx" class="tag-preview">
+					<router-link :to="getUrl(tag)">
+						<station-tag :tag="tag" />
+					</router-link>
+				</li>
+			</ul>
+		</section>
+		<template v-if="!isLoading">
+			<section v-if="stations">
+				<station-list :stations="stations" />
+			</section>
+			<h2 v-else>No stations found</h2>
+		</template>
+		<h2 v-else>Loading...</h2>
+	</main>
 </template>
 
 <script>
 import stationList from "@/cmps/station-list";
 import stationTag from "@/cmps/station-tag";
 export default {
-  created() {},
-  data() {
-    return {
-      isLoading: false,
-    };
-  },
-  methods: {
-    getUrl(tag) {
-      let url = "/explore/";
+	created() { },
+	data() {
+		return {
+			isLoading: false,
+		};
+	},
+	methods: {
+		getUrl(tag) {
+			let url = "/explore/";
 
-      if (this.name) {
-        url += this.name;
-      } else url += "*";
-      if (tag === "All") tag = "";
-      url += "/" + tag;
+			if (this.name) {
+				url += this.name;
+			} else url += "*";
+			if (tag === "All") tag = "";
+			url += "/" + tag;
 
-      return url;
-    },
-    // getStationsByTag(tag){
-    //   return this.stations.filter(station=>{
-    //     return 
-    //   })
-    // }
-  },
-  computed: {
-    name() {
-      return this.$route.params.name;
-    },
-    stations() {
-      return this.$store.getters.stationsToDisplay;
-    },
-    tags() {
-    return this.$store.getters.tags;
-    },
-  },
-  components: {
-    stationList,
-    stationTag,
-  },
-  watch: {
-    "$route.params": {
-      immediate: true,
-      async handler() {
-        const { name, tag = "" } = this.$route.params;
+			return url;
+		},
+		// getStationsByTag(tag){
+		//   return this.stations.filter(station=>{
+		//     return 
+		//   })
+		// }
+	},
+	computed: {
+		name() {
+			return this.$route.params.name;
+		},
+		stations() {
+			return this.$store.getters.stationsToDisplay;
+		},
+		tags() {
+			return this.$store.getters.tags;
+		},
+	},
+	components: {
+		stationList,
+		stationTag,
+	},
+	watch: {
+		"$route.params": {
+			immediate: true,
+			async handler() {
+				const { name, tag = "" } = this.$route.params;
 
-        const filterBy = {
-          name,
-          tag,
-        };
-        if (!name || name === "*") filterBy.name = "";
-        this.$store.commit({ type: "setFilter", filterBy });
-      },
-    },
-  },
+				const filterBy = {
+					name,
+					tag,
+				}
+				if (!name || name === "*") filterBy.name = ""
+				try {
+					this.$store.commit({ type: "setFilter", filterBy })
+				} catch (err) {
+					console.log('Error on set filter =>', err)
+				}
+			},
+		},
+	},
 };
 </script>
