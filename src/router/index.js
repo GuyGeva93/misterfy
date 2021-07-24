bd9300140217798a8ccd6b944bd03a743d800273
Guy Geva <guy1030@gmail.com>
2021-07-24 14:50:07 +0300

    Guy: Heroku ready

--- src/router/index.js
+++ src/router/index.js
@@ -8,39 +8,39 @@ import stationAdd from '../cmps/station-add.vue';
 import signupLogin from '@/views/signup-login.vue';
 Vue.use(VueRouter)
 const routes = [{
-        path: '/',
-        name: 'home',
-        component: home
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
-        path: '/explore/:name?/:tag?',
-        component: explore
-    },
-    {
-        path: '/dashboard',
-        component: dashboard
-    },
-    {
-        path: '/details/:stationId',
-        component: stationDetails
-    },
-    {
-        path: '/add',
-        component: stationAdd
-    },
-    {
-        path: '/signup',
-        component: signupLogin
-    },
+    path: '/',
+    name: 'home',
+    component: home
+},
+{
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import( /* webpackChunkName: "about" */ '../views/About.vue')
+},
+{
+    path: '/explore/:name?/:tag?',
+    component: explore
+},
+{
+    path: '/dashboard',
+    component: dashboard
+},
+{
+    path: '/details/:stationId',
+    component: stationDetails
+},
+{
+    path: '/add',
+    component: stationAdd
+},
+{
+    path: '/signup',
+    component: signupLogin
+},
 ]
 
 const router = new VueRouter({
