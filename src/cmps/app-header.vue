53ec88a3b7fb8bc2686cd0819b127e1a5fde0223
Tal Ben David <talbd51@gmail.com>
2021-07-24 22:53:25 +0300

    tal: user name logic

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -6,23 +6,23 @@
       <img class="logo-txt" src="../assets/logo/logo-txt.png" />
     </span>
     <filter-text-search @search="search" />
-    <h2 class="user-greeting" v-if="loggedinUser">Hello {{ loggedinUser.fullname.substr(0, loggedinUser.fullname.indexOf(' '))}}</h2>
+    <h2 class="user-greeting" v-if="loggedinUser">Hello {{ getFirstName }}</h2>
     <div class="hamburger-container">
-    <svg
-      class="hamburger"
-      @click="toggleNavEntry"
-      height="32px"
-      id="Layer_1"
-      style="enable-background: new 0 0 32 32"
-      version="1.1"
-      viewBox="0 0 32 32"
-      width="32px"
-      xml:space="preserve"
-    >
-      <path
-        d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"
-      />
-    </svg>
+      <svg
+        class="hamburger"
+        @click="toggleNavEntry"
+        height="32px"
+        id="Layer_1"
+        style="enable-background: new 0 0 32 32"
+        version="1.1"
+        viewBox="0 0 32 32"
+        width="32px"
+        xml:space="preserve"
+      >
+        <path
+          d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"
+        />
+      </svg>
     </div>
     <nav class="main-nav" :class="{ 'nav-open': isNavBurgerOpen }">
       <filter-text-search @search="search" />
@@ -72,7 +72,7 @@ export default {
       window.scrollTo(0, 0);
     },
     search(name) {
-      if(!name)return;
+      if (!name) return;
       //Preventing duplicated route error
       const currName = this.$route.params.name;
       if (currName && currName === name) return;
@@ -107,6 +107,14 @@ export default {
     },
   },
   computed: {
+    getFirstName() {
+     let firstName = this.loggedinUser.fullname.substr(
+        0,
+        this.loggedinUser.fullname.indexOf(" ")
+      );
+      if(!firstName) return this.loggedinUser.fullname
+      else return firstName
+    },
     tag() {
       return this.$route.params.tag;
     },
